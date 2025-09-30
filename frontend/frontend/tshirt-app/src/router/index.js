ceffa1a2b541bc58060f6b14c9eccf154a37f137
hermann TCHUENTE <hermannkamsu@gmail.com>
2025-09-30 11:15:09 +0200

    start

---
+++ frontend/frontend/tshirt-app/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Category from '../views/Category.vue'
+import ProductDetails from '../views/ProductDetails.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/category/:id', name: 'Category', component: Category },
+  { path: '/product/:id', name: 'ProductDetails', component: ProductDetails }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ frontend/frontend/tshirt-app/src/views/ProductDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Détails du produit</h1>
+    <div v-if="product">
+      <img :src="product.image" width="150" />
+      <p><strong>Nom :</strong> {{ product.name }}</p>
+      <p><strong>Marque :</strong> {{ product.brand }}</p>
+      <p><strong>Prix :</strong> {{ product.price }} €</p>
+    </div>
+    <p v-else>Produit introuvable</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: null
+    }
+  },
+  mounted() {
+    const productId = this.$route.params.id
+    const allProducts = [
+      { id: 101, name: "T-shirt Noir", brand: "Nike", price: 20, image: "/img/noir.png" },
+      { id: 102, name: "T-shirt Blanc", brand: "Adidas", price: 25, image: "/img/blanc.png" },
+      { id: 201, name: "T-shirt Rose", brand: "Puma", price: 22, image: "/img/rose.png" }
+    ]
+    this.product = allProducts.find(p => p.id == productId)
+  }
+}
+</script>
