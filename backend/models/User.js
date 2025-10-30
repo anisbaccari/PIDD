14889618e2ec2cf8ce1b7ede77bbc07744c333f0
francklin <pefr0903@student.eafc-uccle.be>
2025-10-30 18:53:05 +0100

    probleme de register resolue

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,9 @@ import { sequelize } from '../database/mysql.js';
 
 export const User = sequelize.define('User', {
   id: { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
-  username: { type: DataTypes.STRING(100), allowNull: false },
-  email: { type: DataTypes.STRING(200), allowNull: false, unique: true },
+  nom: { type: DataTypes.STRING(100), allowNull: true },
+  prenom: { type: DataTypes.STRING(100), allowNull: true },
+  username: { type: DataTypes.STRING(100), allowNull: false, unique: true },
   passwordHash: { type: DataTypes.STRING(200), allowNull: false },
 }, { tableName: 'users' });
+
